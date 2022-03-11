076db746cc800216db873d3035d9a4683272b817
thuitamark <mrkthuita@gmail.com>
2022-03-11 16:39:48 +0300

    My fourth commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+*main - E2ntry point
+*
+*Return: 0 after printing the function
+*/
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}

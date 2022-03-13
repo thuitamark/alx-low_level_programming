8ddd5e53b522f1b0cda17cb102915d7214761d67
thuitamark <mrkthuita@gmail.com>
2022-03-13 21:20:33 +0300

    My eigth task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * and that piece of art is useful" - Dora Korpar
+ * Return: return 1 after program execution
+ */
+int main(void)
+{
+echo $("and that piece of art is useful \" - Dora Korpar, 2015-10-19\n");
+return (1);

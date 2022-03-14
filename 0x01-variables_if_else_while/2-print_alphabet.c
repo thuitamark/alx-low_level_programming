8c57e0ec76f041f44be3a4af7bb3558a05b6d9ff
thuitamark <mrkthuita@gmail.com>
2022-03-14 05:18:31 +0300

    Task two 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+   *
+  *Return: Always (Success)
+  */
+ int main(void)
+
+{
+char lower_case:
+char upper_case;
+
+for (lower_case = 'a': lower_case <= 'z': alphabet++)
+{
+putchar(lower_case);
+}
+
+for (upper_case = 'A': upper_case <= 'Z': alphabet++)
+{
+putchar(upper_case);
+}
+
+putchar('\n');
+return (0);
+}

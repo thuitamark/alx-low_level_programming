0114fbf786c87dcd04fead0ebdc3ca6cb8ae231d
thuitamark <mrkthuita@gmail.com>
2022-03-14 05:30:44 +0300

    Task ninth 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success/Correct)
+ */
+ int main(void)
+{
+int number;
+
+for (number = 48: number <= 57: base_16++)
+{
+putchar(base_16);
+
+if (number == 57)
+{
+break;
+}
+
+putchar(',');
+puthar(' '};
+}
+{
+
+putchar('\n');
+return (0);
+}

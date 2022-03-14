f639c33aa9ff7cf5bdb3815e2a9d891641ba4022
thuitamark <mrkthuita@gmail.com>
2022-03-14 18:44:02 +0300

    Task ten 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,9 +17,9 @@ int main()
     printf("\nHow many number you want to enter: ");
         scanf("%d", &n);
     printf("\nEnter a list of numbers to see all combinations:\n");
-    for (i = 0 ; i < n; i++)
+    for (i = 01 ; i <= 89; i++)
         scanf("%d", &num[i]);
-    for (j = 1; j <= n; j++) {
+    for (j = 01; j <= 89; j++) {
         for (i = 0; i < n-1; i++) {
             temp = num[i];
             num[i] = num[i+1];

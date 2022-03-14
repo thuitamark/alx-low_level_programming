da61246a0a5709d939414a3f5b70c5af5fa1d55f
thuitamark <mrkthuita@gmail.com>
2022-03-14 12:19:06 +0300

    Task three 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,15 +7,15 @@
 int main(void)
 
 {
-char lower_case:
+char lower_case;
 char upper_case;
 
-for (lower_case = 'a': lower_case <= 'z': alphabet++)
+for(lower_case = 'a'; lower_case <= 'z'; alphabet++)
 {
 putchar(lower_case);
 }
 
-for (upper_case = 'A': upper_case <= 'Z': alphabet++)
+for(upper_case = 'A'; upper_case <= 'Z'; alphabet++)
 {
 putchar(upper_case);
 }

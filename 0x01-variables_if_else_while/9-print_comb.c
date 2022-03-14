e655f6a32207d1e2d4a0cc5cb286d75572e6de43
thuitamark <mrkthuita@gmail.com>
2022-03-14 15:44:56 +0300

    Task ninth 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,11 +4,11 @@
  *
  * Return: Always (Success/Correct)
  */
- int main(void)
+int main(void)
 {
 int number;
 
-for (number = 48: number <= 57: base_16++)
+for (number = 48; number <= 57; base_16++)
 {
 putchar(base_16);
 
@@ -20,7 +20,7 @@ break;
 putchar(',');
 puthar(' '};
 }
-{
+
 
 putchar('\n');
 return (0);

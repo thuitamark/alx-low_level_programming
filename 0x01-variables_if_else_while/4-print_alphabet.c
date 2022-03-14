7bef8b1bcbb6af097ec6d0b82c0b0c59edee80c7
thuitamark <mrkthuita@gmail.com>
2022-03-14 12:52:24 +0300

    Task four 4-print_alphabet.c

--- 0x01-variables_if_else_while/4-print_alphabet.c
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -5,7 +5,6 @@
  *Return: Always 0 (Success/correct)
  */
 int main(void)
-
 {
 char alphabet;
 
@@ -13,11 +12,14 @@ for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
 if (alphabet == 'q')
 continue;
+
 else if (alphabet == 'e')
 continue;
 
 putchar(alphabet);
 }
+
 putchar('\n');
+
 return (0);
 }

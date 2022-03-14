7daad912a5a52a1412f5610ae423f2d9e9134d6a
thuitamark <mrkthuita@gmail.com>
2022-03-14 16:00:28 +0300

    Task two 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -15,11 +15,6 @@ for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 putchar(alphabet);
 }
 
-for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
-{
-putchar(alphabet);
-}
-
 putchar('\n');
 return (0);
 }

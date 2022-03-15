886e303f5348ea564a878eeb87bd767f602df032
thuitamark <mrkthuita@gmail.com>
2022-03-15 19:00:25 +0300

    My second taskk  2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
   *print_alphabet_x10 - repeats the print_alphabet 10 yimes.
-  *Reurn: Nothing.
+  *Return: Nothing.
   */
-void print_alphabet_x10(void)
+int main void print_alphabet_x10(void)
 {
 
 int i;
@@ -14,9 +14,9 @@ for (i = 0; i < 10; ++i)
 
 for (c = 'a'; c <= 'z'; c++)
 {
-_putchar(c);
+putchar(c);
 }
-_putchar('\n');
+putchar('\n');
 }
 
 }

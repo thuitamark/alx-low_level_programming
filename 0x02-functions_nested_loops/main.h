a8d6a55076b427a5a18b234fcd5cabbd9df3c6f1
thuitamark <mrkthuita@gmail.com>
2022-03-16 04:57:18 +0300

    My main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef MARK_H
-#define MARK_H
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -13,6 +10,3 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n);
-
-#endif /* MARK_H */

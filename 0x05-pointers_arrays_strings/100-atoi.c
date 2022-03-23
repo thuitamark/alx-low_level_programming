239c056d4086aaa7e21528a44c52b13e85cef362
thuitamark <mrkthuita@gmail.com>
2022-03-23 04:44:34 +0300

    My commit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,12 +1,32 @@
 #include "holberton.h"
-
 /**
- * _atoi - Entry point
- * @s: input
- * Return: Always 0 (Success)
- */
+* _atoi - Convert string to an integer.
+* @s: Pointer to a character string.
+*
+* Return: void.
+*/
+
 int _atoi(char *s)
 {
+	int sign;
+	unsigned int num;
+	char *temp;
 
-	return (0);
+	temp = s;
+	num = 0;
+	sign = 1;
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if (*temp == '-')
+			sign *= -1;
+		temp++;
+	}
+	if (*temp != '\0')
+	{
+		do {
+			num = num * 10 + (*temp - '0');
+			temp++;
+		} while (*temp >= '0' && *temp <= '9');
+	}
+	return (num * sign);
 }

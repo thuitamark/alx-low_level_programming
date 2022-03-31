5b97cb1497347e51395202ce564ef809dc0484b5
thuitamark <mrkthuita@gmail.com>
2022-03-31 20:49:39 +0300

    My commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,29 +1,31 @@
 #include "holberton.h"
 
 /**
- * sqrt2 - Makes possible to evaluate from 1 to n
+ * prime2 - Makes possible to evaluate from 1 to n
  * @a: same number as n
  * @b: number that iterates from 1 to n
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int sqrt2(int a, int b)
+int prime2(int a, int b)
 {
-	if (b * b == a)
-		return (b);
-	else if (b * b > a)
-		return (-1);
-	return (sqrt2(a, b + 1));
+	if (a == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+	return (prime2(a, b + 1));
 }
 /**
- * _sqrt_recursion - returns the natural square root of n
+ * is_prime_number - checks if a number is prime
  * @n: Number Integer
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _sqrt_recursion(int n)
+int is_prime_number(int n)
 {
-	return (sqrt2(n, 1));
+	if (n <= 1)
+		return (0);
+	return (prime2(n, 2));
 }
